668c3933ec7948fb7348922ee81260c922a96303
maciekzdyb <maciekzdyb@gmail.com>
2023-04-23 15:01:19 +0200

    refactor using components, @Input, @output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RankingComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
